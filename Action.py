7bc739b03b27e15b47c6ec0be6ac8e62fab58c3d
Rahini2707 <rahinik2707@gmail.com>
2025-05-23 17:16:32 +0530

    Add files via upload

---
+++ Action.py
@@ -0,0 +1,12 @@
+from sema4ai.actions import action
+
+@action
+
+def greet() -> str:
+	"""
+	An Empty AI Action Template.
+
+	Returns:
+		Simple "Hello World Message"
+	"""
+	return "Hello World!\n"
